c347b09c20b47bca788e195cce79a06f7bf5d616
Jorge Junior <jorgedjr21@gmail.com>
2020-05-07 22:25:53 -0300

    adds the final result

--- account_movement.rb
+++ account_movement.rb
@@ -1,20 +1,13 @@
 require_relative 'bank'
 
-def process_accounts(accounts)
-  processed_accounts = {}
-  accounts.each do |account|
-    processed_accounts["#{account[0]}"] = account[1].to_i
-  end
-
-  processed_accounts
-end
-
 accounts = Bank.open_csv(ARGV[0] || "")
 return print accounts[:message] if accounts[:status] == :error
 
 transactions = Bank.open_csv(ARGV[1] || "", "transacoes")
 return print transactions[:message] if transactions[:status] == :error
 
-
 accounts = Bank.process_accounts(accounts[:data])
+print "--------\n"
 accounts = Bank.process_account_transactons(accounts, transactions[:data])
+print "--------\n\e[1mResultado:\e[0m\n\n"
+Bank.final_account_balances(accounts)
