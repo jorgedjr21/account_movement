d9468ca04940b8d247fd98733c4ee00bcc8fe08b
Jorge Junior <jorgedjr21@gmail.com>
2020-05-07 22:23:39 -0300

    class to handle de bank account and transactions

--- bank.rb
+++ bank.rb
@@ -10,7 +10,7 @@ class Bank
   def self.process_accounts(accounts)
     processed_accounts = {}
     accounts.each do |account|
-      print "Creating \e[1maccount #{account[0]}\e[0m with balance of #{Bank.print_balance(account[1])}\n"
+      print "Criando \e[1mconta #{account[0]}\e[0m com saldo inicial #{Bank.print_balance(account[1])}\n"
       processed_accounts["#{account[0]}"] = account[1].to_i
     end
 
@@ -19,9 +19,16 @@ class Bank
 
   def self.process_account_transactons(accounts, transactions)
     transactions.each do |transaction|
-      print "Processing transaction for account #{transaction[0]}, #{Bank.print_transaction(transaction[1])}\n"
-      accounts["#{transaction[0]}"] += transaction[1].to_i
+      message = "Processando transação da conta #{transaction[0]}, #{Bank.print_transaction(transaction[1])}"
+      accounts["#{transaction[0]}"] += transaction[1].to_i 
+      if accounts["#{transaction[0]}"] < 0  && transaction[1].to_i < 0
+        accounts["#{transaction[0]}"] -= 300
+        message += " (R$ 3.00 de multa aplicado)"
+      end
+      print message+", saldo: #{Bank.print_balance(accounts["#{transaction[0]}"])}\n"
     end
+
+    accounts
   end
 
   def self.print_balance(value)
@@ -33,7 +40,13 @@ class Bank
 
   def self.print_transaction(value)
     value = value.to_f / 100
-    return "\e[1m\e[32mcredit of R$ #{value}\e[39m\e[0m" if value > 0
-    "\e[1m\e[31mR$ debit of R$ #{value}\e[39m\e[0m"
+    return "\e[1m\e[32mdepósito de R$ #{value}\e[39m\e[0m" if value > 0
+    "\e[1m\e[31mdébito de R$ #{value.abs}\e[39m\e[0m"
+  end
+
+  def self.final_account_balances(accounts)
+    accounts.each do |account|
+      puts "#{account[0]},#{account[1]}"
+    end
   end
 end
